3bd4cd3d9744c2a87811e93e3c790afd4f7333fa
mitul013 <mitul.adroja13@gmail.com>
2020-11-19 22:21:39 +0530

    useEffect

--- src/components/cockpit/cockpit.js
+++ src/components/cockpit/cockpit.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const cockpit = props => {
+const Cockpit = (props) => {
 
-   const style={
-      backgroundColor:"lightgreen",
-      font:"inherit",
-      border:"1px solid darkgreen"
-    }
+  useEffect(() => {
+    console.log("[cockpit.js] useEffect");
+    return (()=>{
+      console.log("[cockpit.js] cleaning useEffect");
+    })
+  }, []);
+
+  const style = {
+    backgroundColor: "lightgreen",
+    font: "inherit",
+    border: "1px solid darkgreen"
+  }
 
   return (
-        <div>
-          <h1>React Complete Guide</h1>
-           <button style={style} onClick={()=>props.toggle(props.flag)}>SWITCH</button>
-           {props.persons}
-        </div>
+    <div>
+      <h1>React Complete Guide</h1>
+      <button style={style} onClick={() => props.toggle(props.flag)}>SWITCH</button>
+
+    </div>
   );
 }
 
-export default cockpit;
+export default Cockpit;
--- src/container/App.js
+++ src/container/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Cockpit from '../components/cockpit/cockpit';
 import Persons from '../components/Persons/persons';
 // const App = props => {
-  
+
 //   let [personState,setPersonState] = useState({
 //     persons: [
 //       {name:"Mitul",age:23},
@@ -32,22 +32,23 @@ import Persons from '../components/Persons/persons';
 // }
 
 
-class App extends React.Component{
+class App extends React.Component {
 
   // state property
 
   state = {
     persons: [
-      {id:1,name:"Mayur",age:23},
-      {id:2,name:"Yashpal",age:22}
+      { id: 1, name: "Mayur", age: 23 },
+      { id: 2, name: "Yashpal", age: 22 }
     ],
-    personflag:false
+    personflag: false,
+    cockpitFlag: true
   }
 
   switchNameHandler = (age) => {
-    console.log("was clicked"+this.state.persons[0].name)
+    console.log("was clicked" + this.state.persons[0].name)
 
-  //setState is used for update the state in class component
+    //setState is used for update the state in class component
 
     // this.setState(
     //   {persons:[
@@ -58,73 +59,74 @@ class App extends React.Component{
     //Don't do this --> this.state.persons[1].name="YASHPAL"
   }
 
-  nameChange = (event,pid) => {
-    
-    const personIndex = this.state.persons.findIndex((p)=>p.id===pid)
-    
+  nameChange = (event, pid) => {
+
+    const personIndex = this.state.persons.findIndex((p) => p.id === pid)
+
     let newPersons = [...this.state.persons]
 
     newPersons[personIndex].name = event.target.value
 
     this.setState({
-      persons:[...newPersons]
+      persons: [...newPersons]
     })
   }
 
   togglePerson = (flag) => {
-    console.log("FLAG -> ",flag);
+    console.log("FLAG -> ", flag);
     this.setState({
-      personflag : !flag
+      personflag: !flag
     })
   }
 
-   deletePerson = (index) => {
-      const personsCopy = [...this.state.persons]
-      // splice is used for adding or removing 
-      personsCopy.splice(index,1);
-      console.log(personsCopy)
-      this.setState({persons:personsCopy})
-    }
+  deletePerson = (index) => {
+    const personsCopy = [...this.state.persons]
+    // splice is used for adding or removing 
+    personsCopy.splice(index, 1);
+    console.log(personsCopy)
+    this.setState({ persons: personsCopy })
+  }
 
-  render(){
+  render() {
 
-   
-    
+    let persons = null;
 
-   let persons = null;
-
-    if(this.state.personflag){
+    if (this.state.personflag) {
       persons = (
         <Persons persons={this.state.persons}
           click={this.deletePerson}
-          changed={this.nameChange}  />
-       /* this.state.persons.map((el,index)=>{
-          return <Person name={el.name} 
-                  age={el.age} 
-                  click={()=> this.deletePerson(index)}
-                  key={el.id}
-                  changed = {(event)=>this.nameChange(event,el.id)} />
-       }
-        )*/
-          /*
-           <div>
-              <Person name={this.state.persons[0].name} age={this.state.persons[0].age} changed={this.nameChange}/>
+          changed={this.nameChange} />
+        /* this.state.persons.map((el,index)=>{
+           return <Person name={el.name} 
+                   age={el.age} 
+                   click={()=> this.deletePerson(index)}
+                   key={el.id}
+                   changed = {(event)=>this.nameChange(event,el.id)} />
+        }
+         )*/
+        /*
+         <div>
+            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} changed={this.nameChange}/>
 
-              <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
-              click={()=>this.switchNameHandler(111)}>My hobbie: reading a book</Person> 
-          </div> 
-          */
-        )
+            <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
+            click={()=>this.switchNameHandler(111)}>My hobbie: reading a book</Person> 
+        </div> 
+        */
+      )
     }
 
-     
 
-    return(
-        <div className="App">
-           <Cockpit persons={persons} toggle={this.togglePerson}
-           flag={this.state.personflag}
-           />
-        </div>
+
+    return (
+      <div className="App">
+        <button onClick={() => { this.setState({ cockpitFlag: !this.state.cockpitFlag }) }}>Change Cockpit</button>
+        { this.state.cockpitFlag ?
+          <Cockpit persons={this.state.persons} toggle={this.togglePerson}
+            flag={this.state.personflag}
+          /> : null
+        }
+        {persons}
+      </div>
     )
   }
 }
